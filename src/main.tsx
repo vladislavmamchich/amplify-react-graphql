53eec23194084e48af5bc3b9e703c1e035c9a9df
Vlad <vladislav.mamchich@exilion.com>
2023-03-27 12:29:46 +0300

    added auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
