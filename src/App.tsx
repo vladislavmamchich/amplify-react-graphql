8cab682839e82b565b3ab624870cffe1fce25d7e
Vlad <vladislav.mamchich@exilion.com>
2023-03-27 14:16:50 +0300

    vite fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+// import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import { API, Storage } from "aws-amplify";
 import {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      buffer: "buffer",
+      http: "stream-http",
+      https: "https-browserify",
+      url: "url",
+      util: "util",
+    },
+  },
   plugins: [react()],
-})
+  define: {
+    global: {},
+  },
+});
